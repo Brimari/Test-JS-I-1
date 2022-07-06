7dde150fd42a5ef615d348c487d8816422503cdf
Brimari <sabri.alcanta@gmail.com>
2022-07-06 20:40:11 -0300

    subo

--- JavaScript-3/tareas/tareas.js
+++ JavaScript-3/tareas/tareas.js
@@ -109,25 +109,54 @@ return max
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 19.
   //Escribe tu código aquí
+  let cont = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 10) { // Aquí es mayor a 10, no mayor o igual
+      cont.push(array[i]);
+    }
+  }
+  return cont;
 }
+
 
 function diaDeLaSemana(numeroDeDia) {
   //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
+switch (numeroDeDia) {
+  case 1 : 
+    return  "Es fin de semana";
+    break;
+  case 2 :
+  case 3 : 
+  case 4 : 
+  case 5 : 
+  case 6 : 
+    return "Es dia Laboral";
+    break;
+  case 7 : 
+    return  "Es fin de semana";
+}
 } 
 
 function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  let numero = n;
+  if(numero.toString().startsWith("9")){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
+
 } 
 
 function mesesDelAño(array) {
@@ -137,10 +166,17 @@ function mesesDelAño(array) {
   // Tu código:
 }
 
+
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  nuevoArray= []
+  for (let i = 0; i < 200; i++) {
+    if (array[i] > 100) 
+    nuevoArray.push(array[i]);
+}
+return nuevoArray;
 }
 
 // No modificar nada debajo de esta línea, de lo contrario no correrán los test.
